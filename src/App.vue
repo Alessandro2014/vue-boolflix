3b97e210f44a724cd613262550574d57316a5e98
Alessandro2014 <alessandromerenda22@gmail.com>
2021-09-10 13:07:41 +0200

    inizializzazione progetto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <input type="search" value="" placeholder="Ricerca film">
+      <button>Cerca</button>
+    </header>
+    <main>
+      <div>
+
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    
+  },
+  created() {
+    //${query}
+    axios.get('https://api.themoviedb.org/3/movie/550?api_key=e5fb8db6d95f138adcc64dfab8283c0c').then((res) => {
+      console.log(res);
+    });
   }
 }
 </script>
@@ -22,7 +35,26 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+// UTILS
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+// HEADER
+header {
+  height: 100px;
+  background-color: chocolate;
+  padding: 30px;
+}
+
+input, button {
+  padding: 5px 20px;
+  margin: 0 10px;
+}
+
+
 </style>
